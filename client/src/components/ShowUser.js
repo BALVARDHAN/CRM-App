6d4c0474f3b02dc1f90ef23f918235185d1e177b
Balvardhan Parihar <68677559+BALVARDHAN@users.noreply.github.com>
2024-06-22 13:30:42 +0530

    Now Using <table> to show customer information

--- client/src/components/ShowUser.js
+++ client/src/components/ShowUser.js
@@ -48,95 +48,149 @@ export default function ShowUser({ fetchedData }) {
 
   return (
     <div className="xl:w-10/12  mx-auto flex flex-col justify-center items-center bg-white rounded-md shadow-lg shadow-slate-500">
-      <div className="w-full grid xl:grid-rows-3 p-5 rounded-t-md">
-        <div className="grid md:grid-cols-3 sm:grid-cols-1">
-          <div className="bg-slate-200 p-2 m-2 shadow-sm shadow-black">
-            <p className="font-semibold text-xs text-gray-400">First Name</p>
-            <input
-              className="w-full font-semibold text-lg bg-slate-200 focus:outline-none focus:border-slate-600"
-              name="firstName"
-              value={copyOfFetchedData["firstName"]}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="bg-slate-200 p-2 m-2 shadow-sm shadow-black">
-            <p className="font-semibold text-xs text-gray-400">Last Name</p>
-            <input
-              className="w-full font-semibold text-lg bg-slate-200 focus:outline-none focus:border-slate-600"
-              name="lastName"
-              value={copyOfFetchedData["lastName"]}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="bg-slate-200 p-2 m-2 shadow-sm shadow-black">
-            <p className="font-semibold text-xs text-gray-400">Phone</p>
-            <input
-              className="w-full font-semibold text-lg bg-slate-200 focus:outline-none focus:border-slate-600"
-              name="phoneNumber"
-              value={copyOfFetchedData["phoneNumber"]}
-              onChange={handleInputChange}
-            />
-          </div>
-        </div>
-        <div className="grid md:grid-cols-3 sm:grid-cols-1">
-          <div className="bg-slate-200 p-2 m-2 shadow-sm shadow-black">
-            <p className="font-semibold text-xs text-gray-400">Email</p>
-            <input
-              className="w-full font-semibold text-lg bg-slate-200 focus:outline-none focus:border-slate-600"
-              name="email"
-              value={copyOfFetchedData["email"]}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="bg-slate-200 p-2 m-2 shadow-sm shadow-black">
-            <p className="font-semibold text-xs text-gray-400">Street</p>
-            <input
-              className="w-full font-semibold text-lg bg-slate-200 focus:outline-none focus:border-slate-600"
-              name="street"
-              value={copyOfFetchedData["street"]}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="bg-slate-200 p-2 m-2 shadow-sm shadow-black">
-            <p className="font-semibold text-xs text-gray-400">State</p>
-            <input
-              className="w-full font-semibold text-lg bg-slate-200 focus:outline-none focus:border-slate-600"
-              name="state"
-              value={copyOfFetchedData["state"]}
-              onChange={handleInputChange}
-            />
-          </div>
-        </div>
-        <div className="grid md:grid-cols-3 sm:grid-cols-1">
-          <div className="bg-slate-200 p-2 m-2 shadow-sm shadow-black">
-            <p className="font-semibold text-xs text-gray-400">City</p>
-            <input
-              className="w-full font-semibold text-lg bg-slate-200 focus:outline-none focus:border-slate-600"
-              name="city"
-              value={copyOfFetchedData["city"]}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="bg-slate-200 p-2 m-2 shadow-sm shadow-black">
-            <p className="font-semibold text-xs text-gray-400">Postal Code</p>
-            <input
-              className="w-full font-semibold text-lg bg-slate-200 focus:outline-none focus:border-slate-600"
-              name="postalCode"
-              value={copyOfFetchedData["postalCode"]}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="bg-slate-200 p-2 m-2 shadow-sm shadow-black">
-            <p className="font-semibold text-xs text-gray-400">Organization</p>
-            <input
-              className="w-full font-semibold text-lg bg-slate-200 focus:outline-none focus:border-slate-600"
-              name="currentOrganization"
-              value={copyOfFetchedData["currentOrganization"]}
-              onChange={handleInputChange}
-            />
-          </div>
-        </div>
-      </div>
+      <table className=" w-full flex flex-col gap-2 mb-10">
+        <tr className="flex gap-2">
+          <td className="basis-1/3 bg-slate-200 py-2 px-2">
+            <p className="text-sm text-stone-500">First Name</p>
+            {isEditing ? (
+              <input
+                className="w-full font-semibold text-lg bg-white border border-slate-400 px-2 py-2 focus:outline-none focus:border-slate-600"
+                name="firstName"
+                value={copyOfFetchedData["firstName"]}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <p className="text-xl font-semibold">
+                {copyOfFetchedData["firstName"]}
+              </p>
+            )}
+          </td>
+          <td className="basis-1/3 bg-slate-200 py-2 px-2">
+            <p className="text-sm text-stone-500">Last Name</p>
+            {isEditing ? (
+              <input
+                className="w-full font-semibold text-lg bg-white border border-slate-400 px-2 py-2 focus:outline-none focus:border-slate-600"
+                name="lastName"
+                value={copyOfFetchedData["lastName"]}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <p className="text-xl font-semibold">
+                {copyOfFetchedData["lastName"]}
+              </p>
+            )}
+          </td>
+          <td className="basis-1/3 bg-slate-200 py-2 px-2">
+            <p className="text-sm text-stone-500">Phone</p>
+            {isEditing ? (
+              <input
+                className="w-full font-semibold text-lg bg-white border border-slate-400 px-2 py-2 focus:outline-none focus:border-slate-600"
+                name="phoneNumber"
+                value={copyOfFetchedData["phoneNumber"]}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <p className="text-xl font-semibold">
+                {copyOfFetchedData["phoneNumber"]}
+              </p>
+            )}
+          </td>
+        </tr>
+        <tr className="flex gap-2">
+          <td className="basis-1/3 bg-slate-200 py-2 px-2">
+            <p className="text-sm text-stone-500">Email</p>
+            {isEditing ? (
+              <input
+                className="w-full font-semibold text-lg bg-white border border-slate-400 px-2 py-2 focus:outline-none focus:border-slate-600"
+                name="email"
+                value={copyOfFetchedData["email"]}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <p className="text-xl font-semibold">
+                {copyOfFetchedData["email"]}
+              </p>
+            )}
+          </td>
+          <td className="basis-1/3 bg-slate-200 py-2 px-2">
+            <p className="text-sm text-stone-500">Street</p>
+            {isEditing ? (
+              <input
+                className="w-full font-semibold text-lg bg-white border border-slate-400 px-2 py-2 focus:outline-none focus:border-slate-600"
+                name="street"
+                value={copyOfFetchedData["street"]}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <p className="text-xl font-semibold">
+                {copyOfFetchedData["street"]}
+              </p>
+            )}
+          </td>
+          <td className="basis-1/3 bg-slate-200 py-2 px-2">
+            <p className="text-sm text-stone-500">State</p>
+            {isEditing ? (
+              <input
+                className="w-full font-semibold text-lg bg-white border border-slate-400 px-2 py-2 focus:outline-none focus:border-slate-600"
+                name="state"
+                value={copyOfFetchedData["state"]}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <p className="text-xl font-semibold">
+                {copyOfFetchedData["state"]}
+              </p>
+            )}
+          </td>
+        </tr>
+        <tr className="flex gap-2">
+          <td className="basis-1/3 bg-slate-200 py-2 px-2">
+            <p className="text-sm text-stone-500">City</p>
+            {isEditing ? (
+              <input
+                className="w-full font-semibold text-lg bg-white border border-slate-400 px-2 py-2 focus:outline-none focus:border-slate-600"
+                name="city"
+                value={copyOfFetchedData["city"]}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <p className="text-xl font-semibold">
+                {copyOfFetchedData["city"]}
+              </p>
+            )}
+          </td>
+          <td className="basis-1/3 bg-slate-200 py-2 px-2">
+            <p className="text-sm text-stone-500">Postal Code</p>
+            {isEditing ? (
+              <input
+                className="w-full font-semibold text-lg bg-white border border-slate-400 px-2 py-2 focus:outline-none focus:border-slate-600"
+                name="postalCode"
+                value={copyOfFetchedData["postalCode"]}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <p className="text-xl font-semibold">
+                {copyOfFetchedData["postalCode"]}
+              </p>
+            )}
+          </td>
+          <td className="basis-1/3 bg-slate-200 py-2 px-2">
+            <p className="text-sm text-stone-500">Organization</p>
+            {isEditing ? (
+              <input
+                className="w-full font-semibold text-lg bg-white border border-slate-400 px-2 py-2 focus:outline-none focus:border-slate-600"
+                name="currentOrganization"
+                value={copyOfFetchedData["currentOrganization"]}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <p className="text-xl font-semibold">
+                {copyOfFetchedData["currentOrganization"]}
+              </p>
+            )}
+          </td>
+        </tr>
+      </table>
       <div className="w-full flex sm:flex-row flex-col justify-between items-center pb-5 px-5 gap-2">
         <p
           className={`${
